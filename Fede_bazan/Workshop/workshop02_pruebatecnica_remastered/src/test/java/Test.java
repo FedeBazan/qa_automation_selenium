98dc92d59e800a6ffb8c9aa0d19572a3ae8172d7
The Fedenator <48597230+FedeBazan@users.noreply.github.com>
2024-01-11 03:10:58 -0300

    UP TRY

--- Fede_bazan/Workshop/workshop02_pruebatecnica_remastered/src/test/java/Test.java
+++ Fede_bazan/Workshop/workshop02_pruebatecnica_remastered/src/test/java/Test.java
@@ -3,7 +3,6 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeTest;
 import testingyes.*;
 
